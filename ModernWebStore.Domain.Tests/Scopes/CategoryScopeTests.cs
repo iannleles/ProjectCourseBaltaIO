6891528f5d89f1d0248dfe4b0d8556f57cbf0025
Iann Leles <iannleles@outlook.com>
2020-08-30 23:56:11 -0300

    Testando Registro e atualização de categoria

---
+++ ModernWebStore.Domain.Tests/Scopes/CategoryScopeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModernWebStore.Domain.Entities;
+using ModernWebStore.Domain.Scopes;
+
+namespace ModernWebStore.Domain.Tests.Scopes
+{
+    [TestClass]
+    public class CategoryScopeTests
+    {
+        [TestMethod]
+        [TestCategory("Category")]
+        public void ShouldRegisterCategory()
+        {
+            var category = new Category("Placa Mãe");
+            category.Register();
+
+            Assert.AreEqual(true, CategoryScopes.CreateCategoryScopeIsValid(category));
+        }
+
+        [TestMethod]
+        [TestCategory("Category")]
+        public void ShouldUpdateCategoryTitle()
+        {
+            var category = new Category("Placa Mãe");
+            category.Register();
+
+            Assert.AreEqual(true, CategoryScopes.UpdateCategoryScopeIsValid(category, "Motherboards"));
+        }
+    }
+}
