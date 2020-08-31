1352a1a012faed38831ad67b523db6e8226d2c33
Iann Leles <iannleles@outlook.com>
2020-08-30 22:28:54 -0300

    DomainEvents , DomainNotification

---
+++ ModernWebStore.SharedKernel/Events/DomainNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.SharedKernel.Events
+{
+    public class DomainNotification
+    {
+        public string Key{ get; private set; }
+        public string Value { get; private set; }
+        public DateTime DateOcurred { get; private set; }
+        
+        public DomainNotification(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.DateOcurred = DateTime.Now;
+        }
+    }
+}
