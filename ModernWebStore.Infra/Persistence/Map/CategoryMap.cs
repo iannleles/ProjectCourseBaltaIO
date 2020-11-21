25f46c1dde910a6e48d66a3e8895c889ddb4a736
Iann Leles <iannleles@outlook.com>
2020-11-21 00:16:09 -0300

    Criando DataContexts com mapeamento das entidades e instalando EntityFramework no projeto de Infra

---
+++ ModernWebStore.Infra/Persistence/Map/CategoryMap.cs
@@ -0,0 +1,24 @@
+﻿using ModernWebStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.Infra.Persistence.Map
+{
+    public class CategoryMap : EntityTypeConfiguration<Category>
+    {
+        public CategoryMap()
+        {
+            ToTable("Categoy");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Title)
+                .HasMaxLength(60)
+                .IsRequired();
+        }
+    }
+}
---
+++ ModernWebStore.Infra/Persistence/Map/UserMap.cs
@@ -0,0 +1,32 @@
+﻿using ModernWebStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWebStore.Infra.Persistence.Map
+{
+    public class UserMap : EntityTypeConfiguration<User>
+    {
+        public UserMap()
+        {
+            ToTable("User");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Email)
+                .HasMaxLength(160)
+                .IsRequired();
+
+            Property(x => x.Password)
+                .HasMaxLength(32)
+                .IsFixedLength()
+                .IsRequired();
+
+            Property(x => x.IsAdmin)
+                .IsRequired();
+        }
+    }
+}
